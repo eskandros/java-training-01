5341de26b1da0fe5b3099a8a9843c14b8f8e51b9
eskandros <mseskandros@bigpond.com>
2018-04-06 00:51:57 +1000

    Spring Microservice

---
+++ src/main/java/com/training/spring/JavaTraining01Application.java
@@ -0,0 +1,21 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaTraining01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTraining01Application.class, args);
+	}
+	
+	@RequestMapping(value="/get", method=RequestMethod.GET)
+	public String get() {
+		return "Resturn from the Spring Microservice project ...";
+	}
+}
